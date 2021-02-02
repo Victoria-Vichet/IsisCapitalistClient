56a4067fb272bf2d51747c6611c0c12047c3693a
Victoria V <victoria.vichet@gmail.com>
2021-02-02 12:29:36 +0100

    Création des premiers composants et du service de communication avec le service web OK

--- src/app/restservice.service.ts
+++ src/app/restservice.service.ts
@@ -6,5 +6,30 @@ import { World, Pallier, Product } from './world';
   providedIn: 'root'
 })
 export class RestserviceService {
+  private server = 'http://localhost:8080/';
+  #user = '';
 
+  constructor(private http: HttpClient) { }
+
+  get user(): string {
+    return this.#user;
+  }
+
+  set user(value: string) {
+    this.#user = value;
+  }
+
+
+  getServer(): string {
+    return this.server;
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+  getWorld(): Promise<World> {
+    return this.http.get(this.server + 'adventureisis/generic/world')
+      .toPromise().catch(this.handleError);
+  }
 }
